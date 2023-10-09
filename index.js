d5b8cdde9ea82620afb08cab29b61e2553e503ee
shivakr07 <kumarshiva0707@gmail.com>
2023-10-10 00:19:05 +0530

    added codes

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const io = socketio(server);
 io.on('connection', (socket) => { // inside the callback we get socket object
     console.log('a user Connected', socket.id);
 
-    socket.on('from_client', () => {
-        console.log('event coming from client');
+    // socket.on('from_client', () => {
+    //     console.log('event coming from client');
+    // })
+
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        // io.emit('msg_rcvd', data);
+        // socket.emit('msg_rcvd', data);
+        socket.broadcast.emit('msg_rcvd', data);
     })
 
-    setInterval(() => {
-        socket.emit('from_server');
-    }, 2000)
+    // setInterval(() => {
+    //     socket.emit('from_server');
+    // }, 2000)
 });
 
 app.use('/', express.static(__dirname + '/public'));
--- public/script.js
+++ public/script.js
@@ -1,14 +1,34 @@
 var socket = io();
 
 let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist');
+
+
+
 btn.onclick = function exec() {
-    socket.emit('from_client');
+    socket.emit('msg_send', {
+        msg: inputMsg.value
+    });
 }
 
-socket.on('from_server', () => {
-    console.log('collected a new event from the server');
-    const div = document.createElement('div');
-    div.innerText =  'New event from server';
-    console.log(div);
-    document.body.appendChild(div);
+socket.on('msg_rcv', (data) => {
+    let limsg = document.createElement('li');
+    limsg.innerText = data.msg;
+    msgList.appendChild(limsg);
 })
+
+
+
+// let btn = document.getElementById('btn');
+// btn.onclick = function exec() {
+//     socket.emit('from_client');
+// }
+
+// socket.on('from_server', () => {
+//     console.log('collected a new event from the server');
+//     const div = document.createElement('div');
+//     div.innerText =  'New event from server';
+//     console.log(div);
+//     document.body.appendChild(div);
+// })
