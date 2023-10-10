41abf376c0a894c80eff79460a2e8cb653a8f2d7
shivakr07 <kumarshiva0707@gmail.com>
2023-10-10 20:59:42 +0530

    added chat logic and done

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const express = require("express");
+const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+
+const connect = require('./config/database-config');
+
+const Chat = require('./models/chat');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => { // inside the callback we get socket object
-    console.log('a user Connected', socket.id);
+io.on('connection', (socket) => {
+    socket.on('join_room', (data) => {
+        console.log("joining a room", data.roomid)
+        socket.join(data.roomid);
+    });
 
-    // socket.on('from_client', () => {
-    //     console.log('event coming from client');
-    // })
-
-    socket.on('msg_send', (data) => {
+    socket.on('msg_send', async (data) => {
         console.log(data);
-        // io.emit('msg_rcvd', data);
-        // socket.emit('msg_rcvd', data);
-        socket.broadcast.emit('msg_rcvd', data);
+        const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        });
+        io.to(data.roomid).emit('msg_rcvd', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomid).emit('someone_typing');
     })
-
-    // setInterval(() => {
-    //     socket.emit('from_server');
-    // }, 2000)
 });
-
+app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000, () => {
-    console.log('Server Started');
-}); 
+app.get('/chat/:roomid', async (req, res) => {
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
+    res.render('index', {
+        name: 'Sanket',
+        id: req.params.roomid,
+        chats: chats
+    });
+});
 
-//every socket object have unique id
+server.listen(3000, async () => {
+    console.log('Server started');
+    await connect();
+    console.log("mongo db connected")
+});
--- public/script.js
+++ public/script.js
@@ -4,31 +4,14 @@ let btn = document.getElementById('btn');
 let inputMsg = document.getElementById('newmsg');
 let msgList = document.getElementById('msglist');
 
-
-
 btn.onclick = function exec() {
     socket.emit('msg_send', {
         msg: inputMsg.value
     });
 }
 
-socket.on('msg_rcv', (data) => {
+socket.on('msg_rcvd', (data) => {
     let limsg = document.createElement('li');
     limsg.innerText = data.msg;
     msgList.appendChild(limsg);
 })
-
-
-
-// let btn = document.getElementById('btn');
-// btn.onclick = function exec() {
-//     socket.emit('from_client');
-// }
-
-// socket.on('from_server', () => {
-//     console.log('collected a new event from the server');
-//     const div = document.createElement('div');
-//     div.innerText =  'New event from server';
-//     console.log(div);
-//     document.body.appendChild(div);
-// })
